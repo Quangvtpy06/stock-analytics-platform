4549272af16a0ae0a22e4e36106442a33e37705d
Quangvtpy06 <Vothanhquang.py13@gmail.com>
2026-09-15 00:37:59 +0700

    Delete run_pipeline.bat

--- run_pipeline.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo =======================================================
-echo     Vietnam Stock Analytics - Automated Pipeline
-echo =======================================================
-echo.
-echo Starting the pipeline...
-python -u main.py
-pause
